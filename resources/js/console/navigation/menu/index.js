6f7cfae160b6492b602cdf70c87b8f2496e8b4a6
bsenel <beratcansenel@gmail.com>
2020-10-26 11:03:59 +0100

    Add the console from the edgenet repository

---
+++ resources/js/console/navigation/menu/index.js
@@ -0,0 +1,10 @@
+import MyAuthorityMenu from "./MyAuthorityMenu";
+import MainMenu from "./MainMenu";
+import ProfileMenu from "./ProfileMenu";
+import RequestsMenu from "./RequestsMenu";
+import KubernetesMenu from "./KubernetesMenu";
+import ClusterAdminMenu from "./ClusterAdminMenu";
+
+export {
+    MyAuthorityMenu, MainMenu, ProfileMenu, RequestsMenu, KubernetesMenu, ClusterAdminMenu
+}
---
+++ resources/js/console/resources/index.js
@@ -0,0 +1,9 @@
+import { Authority, AuthorityAddress, AuthorityContact } from "./Authority";
+import { Node } from "./Node";
+import { Slice } from "./Slice";
+import { User } from "./User";
+
+export {
+    Authority, AuthorityAddress, AuthorityContact,
+    Node, Slice, User
+}
