1d0811f03cf75d037ececd6e1ef1b95692cadd3a
Ciro Scognamiglio <ciro@cslash.com>
2024-07-14 17:24:43 +0200

    requests cmd for list/cleaning with scheduling command

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -24,7 +24,7 @@ class Kernel extends ConsoleKernel
      */
     protected function schedule(Schedule $schedule)
     {
-        Schedule::command('edgenet:requests:clean --delete')
+        $schedule->command('edgenet:requests:clean --delete')
             ->everyMinute();
     }
 
