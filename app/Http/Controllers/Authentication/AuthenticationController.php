6d2418bcc7ce311c9bbb7b3262f3dda14a7ced9e
Ciro Scognamiglio <ciro@cslash.com>
2024-07-16 21:15:48 +0200

    added activity log to user authentication

--- app/Http/Controllers/Authentication/AuthenticationController.php
+++ app/Http/Controllers/Authentication/AuthenticationController.php
@@ -30,10 +30,20 @@ class AuthenticationController extends Controller
         $user->lastname = $data['lastname'];
         $user->save();
 
+        activity('auth')
+            //->performedOn($this->team)
+            ->causedBy($user)
+            ->withProperties([
+                'severity' => 'info',
+                'ip' => $request->getClientIp(),
+                'client' => $request->userAgent()
+            ])
+            ->log('User updated successfully');
+
         return response()->json(['message' => 'saved']);
     }
 
-    public function login()
+    public function login(Request $request)
     {
         validator(request()->all(), [
             'email' => ['required', 'email'],
@@ -43,19 +53,51 @@ class AuthenticationController extends Controller
         $user = User::where('email', request('email'))->first();
 
         if (!$user) {
+
+            activity('auth')
+                //->performedOn($this->team)
+                //->causedBy($user)
+                ->withProperties([
+                    'severity' => 'error',
+                    'ip' => $request->getClientIp(),
+                    'client' => $request->userAgent(),
+                    'email' => request('email'),
+                ])
+                ->log('Authentication failed (user)');
+
             return response()->json(['message' => 'authentication failed'], 401);
         }
 
         if (Hash::check(request('password'), $user->getAuthPassword())) {
 
-            $token = $user->createToken('app-admins');
+            $token = $user->createToken('console');
+
+            activity('auth')
+                //->performedOn($this->team)
+                ->causedBy($user)
+                ->withProperties([
+                    'severity' => 'info',
+                    'ip' => $request->getClientIp(),
+                    'client' => $request->userAgent()
+                ])
+                ->log('Authentication succeeded');
 
             return response()->json([
                 'token' => $token->plainTextToken
             ]);
-        }
+        } else {
+            activity('auth')
+                //->performedOn($this->team)
+                ->causedBy($user)
+                ->withProperties([
+                    'severity' => 'error',
+                    'ip' => $request->getClientIp(),
+                    'client' => $request->userAgent()
+                ])
+                ->log('Authentication failed (password)');
 
-        return response()->json(['message' => 'authentication failed'], 401);
+            return response()->json(['message' => 'authentication failed'], 401);
+        }
     }
 
     public function logout()
