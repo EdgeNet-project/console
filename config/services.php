80f75cb4bcd30274d7a56a021c2723b6efbcf072
Ciro Scognamiglio <ciro@cslash.com>
2026-03-11 16:23:36 +0100

    socialite package

--- config/services.php
+++ config/services.php
@@ -30,4 +30,36 @@ return [
         'region' => env('AWS_DEFAULT_REGION', 'us-east-1'),
     ],
 
+    /*
+     * SLICES OIDC authentication provider
+     * https://doc.slices-sc.eu/testbed_owner/oauth.html
+     *
+     * package:
+     * https://github.com/Kovah/laravel-socialite-oidc
+     */
+    'slices' => [
+        'base_url' => env('SLICES_OIDC_BASE_URL'),
+        'client_id' => env('SLICES_OIDC_CLIENT_ID'),
+        'client_secret' => env('SLICES_OIDC_CLIENT_SECRET'),
+        'redirect' => env('SLICES_OIDC_REDIRECT_URI'),
+
+        'scopes' => env('SLICES_OIDC_SCOPES', 'openid profile email'),
+
+        // Optional: Enable JWT signature verification (default: false)
+        'verify_jwt' => env('SLICES_OIDC_VERIFY_JWT', false),
+
+        // Optional: Provide a specific public key for JWT verification
+        // If not provided, the key will be fetched from the OIDC provider's JWKS endpoint
+        'jwt_public_key' => env('SLICES_OIDC_JWT_PUBLIC_KEY'),
+    ],
+
+    /*
+     * GITHUB authentication
+     */
+    'github' => [
+        'client_id' => env('GITHUB_CLIENT_ID'),
+        'client_secret' => env('GITHUB_CLIENT_SECRET'),
+        'redirect' => env('GITHUB_REDIRECT_URI'),
+    ],
+
 ];
