ed79d98d008c8e18dae2e14756525280258c1cca
Ciro Scognamiglio <ciro@cslash.com>
2026-06-04 13:42:59 +0200

    Mail template

---
+++ resources/views/vendor/mail/html/header.blade.php
@@ -0,0 +1,8 @@
+@props(['url'])
+<tr>
+<td class="header">
+<a href="https://console.planetlab.io" style="display: inline-block;" >
+<img src="https://console.planetlab.io/images/planetlab-v.png" height="100" class="logo" alt="PlanetLab">
+</a>
+</td>
+</tr>
